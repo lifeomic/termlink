99fca3b1416be559996b37d7ab12603268d7c13f
Taylor Steinberg <taylor.steinberg@lifeomic.com>
2019-03-22 11:22:07 -0400

    Adds a rate limit calcluation that sleeps between each batch

--- termlink/batches.py
+++ termlink/batches.py
@@ -2,6 +2,8 @@ import time
 
 from termlink.configuration import logger
 
+RATE_LIMIT = 50 # requests per second
+
 def batch(iterable, n=1, sleep=0):
   """ Traverses over an iterable in batches of size n
   
@@ -20,6 +22,11 @@ def batch(iterable, n=1, sleep=0):
       yields a batch of size n len(iterable) % n times
 
   """
+
+  limit = n / (RATE_LIMIT)
+  sleep = sleep if (sleep < limit and sleep > 0) else limit
+  logger.info("sleep set to %f seconds" % sleep)
+
   l = len(iterable)
   for ndx in range(0, l, n):
       yield iterable[ndx:min(ndx + n, l)]
