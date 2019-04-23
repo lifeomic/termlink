9ae93e38be17b61de0ed301902678c8e26600dfa
Taylor Steinberg <taylor.steinberg@lifeomic.com>
2019-04-23 13:11:05 -0400

    Updates the rxnorm sub-command help

--- termlink/__main__.py
+++ termlink/__main__.py
@@ -12,14 +12,34 @@ configuration = Config()
 logger = configuration.logger
 
 parser = argparse.ArgumentParser(
-    description="A utility program for uploading terminologies"
+    description="""
+    An ontology conversion toolkit for the Precision Health Cloud.
+    """
 )
 
-subparsers = parser.add_subparsers(title="Commands", description="", help="")
+subparsers = parser.add_subparsers(
+    title="Commands",
+    metavar=""
+)
 
-parser_rxnorm = subparsers.add_parser("rxnorm", help="Upload an RxNorm dataset")
+parser_rxnorm = subparsers.add_parser(
+    "rxnorm", 
+    help="RxNorm provides normalized names for clinical drugs.",
+    description="""
+    RxNorm provides normalized names for clinical drugs and links its names to
+    many of the drug vocabularies commonly used in pharmacy management and drug
+    interaction software, including those of First Databank, Micromedex, 
+    Gold Standard Drug Database, and Multum. By providing links between these 
+    vocabularies, RxNorm can mediate messages between systems not using the 
+    same software and vocabulary. [1] 
+    """,
+    epilog="""
+    [1] RxNorm. Retrieved April 22, 2019, from https://www.nlm.nih.gov/research/umls/rxnorm/
+    """
+)
 parser_rxnorm.add_argument("uri", metavar="URI", help="resource identifier for files")
 parser_rxnorm.set_defaults(execute=RxNormCommand.execute)
+
 
 args = parser.parse_args()
 args.execute(args)
